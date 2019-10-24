62da862c07f6f438814fd2f7fa3709c321597625
Ian Campbell <bobbleian@gmail.com>
2019-10-24 11:15:07 -0700

    Initial Commit

---
+++ TLSNetworkApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TLSNetworkApp
+//
+//  Created by Ian Campbell on 2019-10-24.
+//  Copyright © 2019 Ian Campbell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
